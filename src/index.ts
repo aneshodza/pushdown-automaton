a0dd4c119200e9da977378869f873850f7fe377e
Anes Hodza <anes.hodza04@gmail.com>
2024-04-18 08:45:23 +0200

    Add improved CI/CD to also deploy to npm

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// src/index.ts
+export { default as PushdownAutomaton } from './PushdownAutomaton';
+export { default as Stack } from './Stack';
+export { default as State } from './State';
+export { default as TransitionFunction } from './TransitionFunction';
